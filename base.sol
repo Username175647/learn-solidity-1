b4f1a8ae6d1c6ab3c8e24f9aee23c131e4a19604
Username175647 <ghostghostgho@yandex.ru>
2026-01-28 20:18:37 +0300

    add pureFunc

--- base.sol
+++ base.sol
@@ -166,4 +166,7 @@ contract Constructor {
         owner = msg.sender;
         correntYear = _currentYear;
     }
+    function pureFunc(uint256 a, uint256 b) public pure returns(uint256) {
+        return a + b;
+    }
 }
